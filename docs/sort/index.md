8991a4d851c627d15c0dea7a1e0af87f7896ee24
DuyThinh1415 <89759103+DuyThinh1415@users.noreply.github.com>
2022-03-27 17:11:47 +0700

    Update Docs of bubble sort and some comment of bubble sort def

--- docs/sort/index.md
+++ docs/sort/index.md
@@ -12,6 +12,18 @@ Các giải thuật sort được thư viện cung cấp
 
 ## Bubble sort
 
+Sắp xếp nổi bọt (tiếng Anh: bubble sort) là một thuật toán sắp xếp đơn giản, với thao tác cơ bản là so sánh hai phần tử kề nhau, nếu chúng chưa đứng đúng thứ tự thì đổi chỗ (swap). Có thể tiến hành từ trên xuống (bên trái sang) hoặc từ dưới lên (bên phải sang). Sắp xếp nổi bọt còn có tên là sắp xếp bằng so sánh trực tiếp. Nó sử dụng phép so sánh các phần tử nên là một giải thuật sắp xếp kiểu so sánh.
+
+Có hai cách thực hiện giải thuật là sắp xếp từ dưới lên hoặc sắp xếp từ trên xuống. Trong thư viện này ta sẽ sắp xếp từ dưới lên (so sánh cặp phần tử index 0 đầu tiên).
+
+### mã giả của giải thuật
+lặp lại công việc sau 
+    i tăng dần từ 0 đến (n - số lần quét - 1)
+        so sánh arr[i] và arr[i+1]
+        nếu nó chưa đúng thứ tự thì đổi chỗ cho nhau 
+    nếu lần quét này không có sự đổi chỗ nào thì kết thúc giải thuật
+    tăng số lần quét lên 1
+
 ## Heap sort
 
 ## Insertion sort
--- myarrayutils/sort/BubbleSort.py
+++ myarrayutils/sort/BubbleSort.py
@@ -4,10 +4,14 @@ from . import utils
 def BubbleSort(arr: list, reverse: bool = False, cmp=None):
     """
     Sort input array with BubbleSort algorithm
+    It has O(n^2) average time complexity and also worst case.
+
+    NOTE: This sorting algorith is NOT STABLE
 
     :param arr: Input array
-    :param reverse:
-    :param cmp:
+    :param reverse: if false or not mentioned, the algorithm will sort from smallest to largest. 
+    If want to sort from largest to smallest, set it to true
+    :param cmp: used to sort more complex data
     """
     if not cmp:
         cmp = utils.defaultComparator
@@ -20,10 +24,17 @@ def BubbleSort(arr: list, reverse: bool = False, cmp=None):
 
 def process_Bubble_Sort(arr, cmp, comparator):
     """
-    this function will run the bubble sort algorithm
+    this function will run the optimization bubble sort algorithm
     """
     has_swapped = True
+    # This is a flag. It check if any swaps were made in the previous iteration.
+    # If no swaps are made, the algorithm should stop
     num_of_iterations = 0
+    # The first time we pass through the list the position n is guaranteed to be the largest element
+    # the second time we pass through the position n-1 is guaranteed to be the second-largest element and so forth
+    # This means that with each consecutive iteration we can look at one less element than before
+    # More precisely, in the k-th iteration, only need to look at the first n - k + 1 elements
+    # num_of_iterations is a variable to do that
 
     while(has_swapped):
         has_swapped = False
