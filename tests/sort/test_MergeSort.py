34d0c3614bd8fbbe53746ccd5139f7415f2c434e
thinh232002 <102164571+thinh232002@users.noreply.github.com>
2022-04-04 10:07:26 +0700

    Update test_MergeSort.py

--- tests/sort/test_MergeSort.py
+++ tests/sort/test_MergeSort.py
@@ -5,9 +5,3 @@ def test_MergeSort_empty_array():
     expected = []
     sort.MergeSort(arr)
     assert arr == expected
-    
-def test_MergeSort_int_array():
-    arr = [1, 4, 77, 5, 6, 12, 24]
-    expected = [1, 4, 5, 6, 12, 24, 77]
-    sort.MergeSort(arr)
-    assert arr == expected
