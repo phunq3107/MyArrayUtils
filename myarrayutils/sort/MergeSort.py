4b2fd08400b79de0023cc85748719bca649fb9ba
thinh232002 <102164571+thinh232002@users.noreply.github.com>
2022-04-04 08:43:06 +0700

    Update MergeSort.py

--- myarrayutils/sort/MergeSort.py
+++ myarrayutils/sort/MergeSort.py
@@ -8,7 +8,7 @@ def MergeSort(arr):
        This sorting algorithm NEED SPACE and STABLE
        :param arr: Input array
        """
-       if len(arr) >1: 
+       if len(arr) > 1: 
               mid = len(arr)//2 # Finding the mid of the array 
               L = arr[:mid] # Dividing the array elements  
               R = arr[mid:] # into 2 halves 
@@ -21,19 +21,18 @@ def MergeSort(arr):
               while i < len(L) and j < len(R): 
                      if L[i] < R[j]:
                             arr[k] = L[i] 
-                     i+= 1
-              else: 
-                       arr[k] = R[j] 
-                     j+= 1
-              k+= 1
+                            i+= 1
+                            else: 
+                                   arr[k] = R[j] 
+                                   j+= 1
+                                   k+= 1
           
               # Checking if any element was left 
               while i < len(L): 
-              arr[k] = L[i] 
-              i+= 1
-              k+= 1
-          
-        while j < len(R): 
-            arr[k] = R[j] 
-            j+= 1
-            k+= 1
+                     arr[k] = L[i] 
+                     i+= 1
+                     k+= 1
+                     while j < len(R): 
+                            arr[k] = R[j] 
+                            j+= 1
+                            k+= 1
