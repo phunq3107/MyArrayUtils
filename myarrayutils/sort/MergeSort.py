fce82727a8b6ac957c4618036e3097811f31dd6c
thinh232002 <102164571+thinh232002@users.noreply.github.com>
2022-04-04 16:30:19 +0700

    Update MergeSort.py

--- myarrayutils/sort/MergeSort.py
+++ myarrayutils/sort/MergeSort.py
@@ -8,11 +8,17 @@ def MergeSort(arr):
        """
        if len(arr) >1:
               mid = len(arr)//2
+              # Finding the mid of the array
               L = arr[:mid]
+              # Dividing the array elements
               R = arr[mid:]
+              # into 2 halves
               MergeSort(L)
+              # Sorting the first half
               MergeSort(R)
+              # Sorting the second half
               i = j = k = 0
+              # Copy data to temp arrays L[] and R[]
               while i < len(L) and j < len(R):
                      if L[i] < R[j]:
                             arr[k] = L[i]
@@ -21,6 +27,7 @@ def MergeSort(arr):
                             arr[k] = R[j]
                             j+= 1
                             k+= 1
+              # Checking if any element was left 
               while i < len(L):
                      arr[k] = L[i]
                      i+= 1
