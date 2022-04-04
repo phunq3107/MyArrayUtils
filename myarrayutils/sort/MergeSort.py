a1a072e65857254e09f0aeff3268daa1deeb95bd
thinh232002 <102164571+thinh232002@users.noreply.github.com>
2022-04-04 09:03:01 +0700

    Update MergeSort.py

--- myarrayutils/sort/MergeSort.py
+++ myarrayutils/sort/MergeSort.py
@@ -1,6 +1,4 @@
 from . import utils
-
-def MergeSort(arr):
        """
        Sort input array with MergeSort algorithm
        MergeSort belongs to the Divide and Conquer algorithm with O(n.(log(n)))
@@ -8,32 +6,26 @@ def MergeSort(arr):
        This sorting algorithm NEED SPACE and STABLE
        :param arr: Input array
        """
-       if len(arr) >1: 
-              mid = len(arr)//2 # Finding the mid of the array 
-              L = arr[:mid] # Dividing the array elements  
-              R = arr[mid:] # into 2 halves 
-              
-              mergeSort(L) # Sorting the first half 
-              mergeSort(R) # Sorting the second half 
-              i = j = k = 0
-          
-        # Copy data to temp arrays L[] and R[] 
-              while i < len(L) and j < len(R): 
-                     if L[i] < R[j]:
-                            arr[k] = L[i] 
-                     i+= 1
-              else: 
-                       arr[k] = R[j] 
-                     j+= 1
-              k+= 1
-          
-              # Checking if any element was left 
-              while i < len(L): 
-              arr[k] = L[i] 
-              i+= 1
-              k+= 1
-          
-        while j < len(R): 
-            arr[k] = R[j] 
-            j+= 1
-            k+= 1
+       def MergeSort(arr):
+              if len(arr) >1:
+                     mid = len(arr)//2
+                     L = arr[:mid]
+                     R = arr[mid:]
+                     mergeSort(L)
+                     mergeSort(R)
+                     i = j = k = 0
+                     while i < len(L) and j < len(R):
+                            if L[i] < R[j]:
+                                   arr[k] = L[i]
+                                   i+= 1
+                            else:
+                                   arr[k] = R[j]
+                                   j+= 1
+                                   k+= 1
+                                   while i < len(L):
+                                          arr[k] = R[j]
+                                          j+= 1
+                                          k+= 1
+                                          
+                                          
+       
