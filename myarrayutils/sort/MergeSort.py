dc10571d8b318f734f50210a70b16569803665b5
thinh232002 <102164571+thinh232002@users.noreply.github.com>
2022-04-05 23:44:51 +0700

    Update MergeSort.py

--- myarrayutils/sort/MergeSort.py
+++ myarrayutils/sort/MergeSort.py
@@ -1,11 +1,24 @@
-def MergeSort(arr):
+from . import utils
+
+def MergeSort(arr: list, reverse: bool = False, cmp=None):
        """
        Sort input array with MergeSort algorithm
        MergeSort belongs to the Divide and Conquer algorithm with O(n.(log(n)))
        average time complexity and also in worst case.
        This sorting algorithm NEED SPACE and STABLE
        :param arr: Input array
+       :param reverse:
+       
+       :param cmp:
        """
+       if not cmp:
+              cmp = utils.defaultComparator
+
+       def comparator(obj1, obj2): return cmp(
+        obj2, obj1) if reverse else cmp(obj1, obj2)
+       mergeSort(arr, comparator)
+       
+def mergeSort(arr, comparator):
        if len(arr) >1:
               mid = len(arr)//2
               # Finding the mid of the array
@@ -13,14 +26,14 @@ def MergeSort(arr):
               # Dividing the array elements
               R = arr[mid:]
               # into 2 halves
-              MergeSort(L)
+              mergeSort(L)
               # Sorting the first half
-              MergeSort(R)
+              mergeSort(R)
               # Sorting the second half
               i = j = k = 0
               # Copy data to temp arrays L[] and R[]
               while i < len(L) and j < len(R):
-                     if L[i] < R[j]:
+                     if comparator(L[i], R[j]) < 0:
                             arr[k] = L[i]
                             i+= 1
                      else:
