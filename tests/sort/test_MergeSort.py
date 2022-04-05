274884121da6f4282d1fb4fc372d21ca767fcae5
thinh232002 <102164571+thinh232002@users.noreply.github.com>
2022-04-06 02:16:37 +0700

    Update test_MergeSort.py

--- tests/sort/test_MergeSort.py
+++ tests/sort/test_MergeSort.py
@@ -14,10 +14,3 @@ def test_MergeSort_empty_array_with_reverse():
     sort.MergeSort(arr, reverse=True)
     assert arr == expected
 
-    
-def test_MergeSort_int_array_with_cmp():
-    arr = [1, 4, 77, 5, 6, 12, 24]
-    expected = [1, 4, 5, 6, 12, 24, 77]
-    def cmp(x, y): return -1 if x < y else 1 if x > y else 0
-    sort.MergeSort(arr, cmp=cmp)
-    assert arr == expected
