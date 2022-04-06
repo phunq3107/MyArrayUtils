d6d95433889df7a30f509116e7564676e4dd650c
ductam1008 <89824408+ductam1008@users.noreply.github.com>
2022-04-06 16:26:15 +0700

    Update ShellSort.py

--- myarrayutils/sort/ShellSort.py
+++ myarrayutils/sort/ShellSort.py
@@ -1,2 +1,27 @@
-def ShellSort():
+from . import utils
+
+def ShellSort(arr: list, reverse: bool = False, cmp=None):
+    if not cmp:
+        cmp = utils.defaultComparator
+
+    def comparator(obj1, obj2): return cmp(
+        obj2, obj1) if reverse else cmp(obj1, obj2)
+
+    process_ShellSort(arr, cmp, comparator)
+
+def process_ShellSort(arr, cmp, comparator):
+    # Rearrange elements at each n/2, n/4, n/8, ... intervals
+    n = len(arr)
+    interval = n // 2
+    while interval > 0:
+        for i in range(interval, n):
+            temp = arr[i]
+            j = i
+            while j >= interval and comparator(arr[j-interval], temp) == 1:
+                arr[j] = arr[j - interval]
+                j -= interval
+
+            arr[j] = temp
+        interval //= 2
+
     pass
