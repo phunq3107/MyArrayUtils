cb3d900d69663c464762b98a772ba8fa70cdf924
ductam1008 <89824408+ductam1008@users.noreply.github.com>
2022-04-07 16:58:54 +0700

    Update ShellSort.py

--- myarrayutils/sort/ShellSort.py
+++ myarrayutils/sort/ShellSort.py
@@ -1,6 +1,21 @@
 from . import utils
 
 def ShellSort(arr: list, reverse: bool = False, cmp=None):
+    """
+        Shell sort is a generalized version of the insertion sort algorithm.
+        It first sorts elements that are far apart from each other
+        and successively reduces the interval between the elements to be sorted.
+
+        Time Complexity: O(n*log n)
+        Auxiliary Space: O(1)
+        NOTE: This sorting algorith is NOT STABLE
+
+        :param arr: Input array
+        :param reverse:if false or not mentioned, the algorithm will sort from smallest to largest.
+        If want to sort from largest to smallest, set it to true
+
+        :param cmp: used to sort more complex data
+        """
     if not cmp:
         cmp = utils.defaultComparator
 
